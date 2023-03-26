3c575a97a7bf64944ba18ff88b1ba63dc09c1278
Eduardo Murillo <128676818+Eduardo-Murillo22@users.noreply.github.com>
2023-03-25 23:13:41 -0700

    Add files via upload

---
+++ 1 Mancala_V1  Jose Turn Function/main.cpp
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: HP Omen
+ *
+ * Created on March 25, 2023, 11:10 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
